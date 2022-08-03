3f32bbff5cadff95a3b920dc54f11cf881a2c1e3
CavBridge <cavner-bridge@outlook.com>
2022-08-03 12:28:05 +0100

    ticket 5

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,8 +1,20 @@
-const { fetchArticlesById } = require("../models/articles");
+const { fetchArticlesById, patchArticleVotes } = require("../models/articles");
 
 exports.getArticlesAndId = (req, res, next) => {
   const { article_id } = req.params;
   fetchArticlesById(article_id)
     .then((article) => res.status(200).send({ article: article[0] }))
     .catch(next);
+};
+
+exports.updateArticleVotes = (req, res, next) => {
+  const { article_id } = req.params;
+  const { inc_votes } = req.body;
+  patchArticleVotes(article_id, inc_votes)
+    .then((article) => {
+      res.status(200).send({ article: article[0] });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
--- models/articles.js
+++ models/articles.js
@@ -11,3 +11,17 @@ exports.fetchArticlesById = (article_id) => {
       return rows;
     });
 };
+
+exports.patchArticleVotes = (article_id, newVotes = 0) => {
+  return db
+    .query("UPDATE articles SET votes = $1 WHERE article_id = $2 RETURNING *", [
+      newVotes,
+      article_id,
+    ])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "article not found" });
+      }
+      return rows;
+    });
+};
